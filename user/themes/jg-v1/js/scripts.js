3a96af588ef653e54244a036ff34fa1b8308a487
Jon Glick <jonathanpglick@gmail.com>
2016-10-11 07:59:03 -0700

    Adds animated background.

--- user/themes/jg-v1/js/scripts.js
+++ user/themes/jg-v1/js/scripts.js
@@ -7,6 +7,29 @@ function main() {
   inlineCodes.forEach(function(inlineCode) {
     hljs.highlightBlock(inlineCode);
   });
+
+  var scroll = window.requestAnimationFrame ||
+               window.webkitRequestAnimationFrame ||
+               window.mozRequestAnimationFrame ||
+               window.msRequestAnimationFrame ||
+               window.oRequestAnimationFrame ||
+               function(callback){ window.setTimeout(callback, 1000/60) };
+
+  var lastTop = -1;
+  var background1 = document.querySelector('.Layout-background-1');
+  var background1Pos = parseInt(getComputedStyle(background1).backgroundPosition.split(' ')[0], 10);
+  var background2 = document.querySelector('.Layout-background-2');
+  var background2Pos = parseInt(getComputedStyle(background2).backgroundPosition.split(' ')[0], 10);
+  function animationLoop() {
+    var top = window.pageYOffset;
+    if (top != lastTop) {
+      background1.style.backgroundPosition = "0 " + (background1Pos + (0.8 * top)) + "px";
+      background2.style.backgroundPosition = "" + background2Pos + "px " + (background2Pos + (0.85 * top)) + "px";
+    }
+    scroll(animationLoop);
+  }
+
+  animationLoop();
 }
 
 function toArray(a) {
